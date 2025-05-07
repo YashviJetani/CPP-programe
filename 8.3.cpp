39af79067393ae109d347c5276283be20ad737ef
YashviJetani <yashvijetani7@gmail.com>
2025-05-07 22:39:42 +0530

    Add files via upload

---
+++ 8.3.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <set>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n, id;
+    cout << "Enter number of transaction IDs: ";
+    cin >> n;
+
+    set<int> uniqueIDs;
+
+    cout << "Enter " << n << " transaction IDs:\n";
+    for (int i = 0; i < n; ++i) {
+        cin >> id;
+        uniqueIDs.insert(id);
+    }
+
+    cout << "\nUnique Transaction IDs (sorted):\n";
+    for (auto it = uniqueIDs.begin(); it != uniqueIDs.end(); ++it) {
+        cout << *it << endl;
+    }
+    return 0;
+}
